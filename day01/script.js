e71e33358d258ebc04638d259874790f190e54b0
munsohyeon <msh1787@gmail.com>
2025-05-28 13:21:36 +0900

    again html.css.javascript

---
+++ day01/script.js
@@ -0,0 +1,22 @@
+const $screen = document.querySelector("#screen");
+const $toggleButton = document.querySelector("#button");
+let timeInterval;
+let stopWatchOn = false;
+let seconds = 0;
+
+console.log("screen", $screen);
+console.log("toggleButton", $toggleButton);
+
+if ($screen && $toggleButton) {
+  // 첫번째 아규먼트: 문자열(이벤트 이름)
+  // 두번째 아규먼트: 함수 주소값 >> 콜백함수
+  function clickBtn() {
+    alert("하하");
+  }
+
+  const cb = clickBtn;
+  $toggleButton.addEventListener("click", () => {
+    stopWatchOn = !stopWatchOn;
+    console.log("stopWatchOn:", stopWatchOn);
+  });
+}
